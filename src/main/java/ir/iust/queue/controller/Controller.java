fe98cdf28e207035218846498a0482550a2198e7
qhazzale <g.bakhtiari@tourismit.ir>
2021-02-05 23:19:01 +0330

    Edit controller and add documentation

--- src/main/java/ir/iust/queue/controller/Controller.java
+++ src/main/java/ir/iust/queue/controller/Controller.java
@@ -41,29 +41,29 @@ public class Controller {
 
         pces_queue1.initializeSystemParameters();
         tasks1 = pces_queue1.run();
-//
-//        // p = 0.1
-//        pces_queue2.initializeSystemParameters(tasks1, 0.1);
-//        tasks2 = pces_queue2.run();
-//        measureResponseTime(0.1);
-//
-////         p = 0.2
-//        System.out.println("---------------------------------------- probabilities 2 -------------------------------------------------------------");
-//        pces_queue2.initializeSystemParameters(tasks1, 0.2);
-//        tasks2 = pces_queue2.run();
-//        measureResponseTime(0.2);
-//
-//        // p = 0.3
-//        System.out.println("---------------------------------------- probabilities 3 -------------------------------------------------------------");
-//        pces_queue2.initializeSystemParameters(tasks1, 0.3);
-//        tasks2 = pces_queue2.run();
-//        measureResponseTime(0.3);
-//
-//        // p = 0.4
-//        System.out.println("---------------------------------------- probabilities 4 -------------------------------------------------------------");
-//        pces_queue2.initializeSystemParameters(tasks1, 0.4);
-//        tasks2 = pces_queue2.run();
-//        measureResponseTime(0.4);
+
+        // p = 0.1
+        pces_queue2.initializeSystemParameters(tasks1, 0.1);
+        tasks2 = pces_queue2.run();
+        measureResponseTime(0.1);
+
+//         p = 0.2
+        System.out.println("---------------------------------------- probabilities 2 -------------------------------------------------------------");
+        pces_queue2.initializeSystemParameters(tasks1, 0.2);
+        tasks2 = pces_queue2.run();
+        measureResponseTime(0.2);
+
+        // p = 0.3
+        System.out.println("---------------------------------------- probabilities 3 -------------------------------------------------------------");
+        pces_queue2.initializeSystemParameters(tasks1, 0.3);
+        tasks2 = pces_queue2.run();
+        measureResponseTime(0.3);
+
+        // p = 0.4
+        System.out.println("---------------------------------------- probabilities 4 -------------------------------------------------------------");
+        pces_queue2.initializeSystemParameters(tasks1, 0.4);
+        tasks2 = pces_queue2.run();
+        measureResponseTime(0.4);
 //
         // p = 0.5
         System.out.println("---------------------------------------- probabilities 5 -------------------------------------------------------------");
@@ -71,29 +71,29 @@ public class Controller {
         tasks2 = pces_queue2.run();
         measureResponseTime(0.5);
 
-//        // p = 0.6
-//        System.out.println("---------------------------------------- probabilities 6 -------------------------------------------------------------");
-//        pces_queue2.initializeSystemParameters(tasks1, 0.6);
-//        tasks2 = pces_queue2.run();
-//        measureResponseTime(0.6);
-//
-//        // p = 0.7
-//        System.out.println("---------------------------------------- probabilities 7 -------------------------------------------------------------");
-//        pces_queue2.initializeSystemParameters(tasks1, 0.7);
-//        tasks2 = pces_queue2.run();
-//        measureResponseTime(0.7);
-//
-//        // p = 0.8
-//        System.out.println("---------------------------------------- probabilities 8 -------------------------------------------------------------");
-//        pces_queue2.initializeSystemParameters(tasks1, 0.8);
-//        tasks2 = pces_queue2.run();
-//        measureResponseTime(0.8);
-//
-//        // p = 0.9
-//        System.out.println("---------------------------------------- probabilities 9 -------------------------------------------------------------");
-//        pces_queue2.initializeSystemParameters(tasks1, 0.9);
-//        tasks2 = pces_queue2.run();
-//        measureResponseTime(0.9);
+        // p = 0.6
+        System.out.println("---------------------------------------- probabilities 6 -------------------------------------------------------------");
+        pces_queue2.initializeSystemParameters(tasks1, 0.6);
+        tasks2 = pces_queue2.run();
+        measureResponseTime(0.6);
+
+        // p = 0.7
+        System.out.println("---------------------------------------- probabilities 7 -------------------------------------------------------------");
+        pces_queue2.initializeSystemParameters(tasks1, 0.7);
+        tasks2 = pces_queue2.run();
+        measureResponseTime(0.7);
+
+        // p = 0.8
+        System.out.println("---------------------------------------- probabilities 8 -------------------------------------------------------------");
+        pces_queue2.initializeSystemParameters(tasks1, 0.8);
+        tasks2 = pces_queue2.run();
+        measureResponseTime(0.8);
+
+        // p = 0.9
+        System.out.println("---------------------------------------- probabilities 9 -------------------------------------------------------------");
+        pces_queue2.initializeSystemParameters(tasks1, 0.9);
+        tasks2 = pces_queue2.run();
+        measureResponseTime(0.9);
     }
     public void measureResponseTime(double probability){
         double time = 0.0;
